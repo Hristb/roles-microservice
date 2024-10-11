925084f1c6132b8bfd3652420363cd148f820475
Hrist Joy <hristbartra@gmail.com>
2024-10-11 17:57:26 -0500

    first commit

---
+++ src/main/java/com/hrist/roles_microservice/RolesMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.hrist.roles_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolesMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RolesMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrist/roles_microservice/RolesMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrist.roles_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolesMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
